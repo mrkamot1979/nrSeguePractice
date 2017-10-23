304ce6f934dcaec4cc858cc003a74c01257e7d79
nenu <nino_roldan@yahoo.com>
2017-10-23 19:55:44 +0900

    Initial Commit

---
+++ nrSeguePractice/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  nrSeguePractice
+//
+//  Created by Nino Rorudan on 10/23/17.
+//  Copyright © 2017 The NTMC Foundation. All rights reserved.
+//
+
+
+
+import UIKit
+
+var enteredString:String = ""
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
